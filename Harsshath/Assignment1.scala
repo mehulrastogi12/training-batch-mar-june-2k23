36228b0451f05fdd7506746c38049819d83d9ab0
Harsshath23 <harsshath0819@gmail.com>
2023-03-13 14:33:10 +0530

    Modified the code using recursive function

--- Harsshath/Assignment1.scala
+++ Harsshath/Assignment1.scala
@@ -1,17 +1,27 @@
 object Assignment1 extends App{
-  println("Size of the Array")
+  def countPerfectSquares(arr: Array[Int], n: Int): Int = {
+    if (n == 0) {
+      0
+    } else {
+      val Count = countPerfectSquares(arr.tail, n - 1)
+      val s = scala.math.sqrt(arr.head)
+      if (s == s.toInt) {
+        Count + 1
+      } else {
+        Count
+      }
+    }
+  }
+
+  println("Size of the array")
   val n = scala.io.StdIn.readInt()
-  var arr = new Array[Int](n)
-  println("Elements of the Array")
-  var count = 0
+  val arr = new Array[Int](n)
+  println("Elements")
   for (i <- 0 to n - 1) {
     val x = scala.io.StdIn.readInt()
     arr(i) = x
-    val s = scala.math.sqrt(x)
-    if (s == s.toInt) {
-      count = count + 1
-    }
   }
-  println(s"Count: $count")
+  val Count = countPerfectSquares(arr, n)
+  println(s"Count: $Count")
 
 }
