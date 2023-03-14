6664d17ced0c1fcc1dc7e2ac9b995f120d44fa2f
zodapeharshal <zodape_harshal@rediffmail.com>
2023-03-14 19:51:02 +0530

    added assignment 2

--- src/main/scala/MainClass.scala
+++ src/main/scala/MainClass.scala
@@ -11,6 +11,27 @@ object MainClass {
     factorial(x-1, x * fac) ;
   }
 
+
+  // Time complexity : O(n)
+  // space Complexity : O(n) : stack space
+  @tailrec
+  private  def fibonacci(n: Int, a:Int, b:Int ):Int = {
+    if(n==0)
+      return b ;
+    fibonacci(n-1, b , a+b ) ;
+  }
+  private def getFibonacciNumber(): Unit = {
+    print("Enter the n to get nth fibonacci number : " ) ;
+    var n : Int = scala.io.StdIn.readInt()
+    if(n == 1) {
+      println(s"1st fibonacci number is : 0")
+    }
+    else if (n== 2){
+      println(s"2nd Fibonacci number is : 1");
+    }else{
+      println(s"${n}th fibonacci number is ${fibonacci(n-2,0,1)}") ;
+    }
+  }
   private def findPerfectSquare(): Unit = {
     print("Enter array Values : ")
     val arr = readLine().split(" ").map(_.toInt)
@@ -25,5 +46,6 @@ object MainClass {
   def main(args: Array[String]): Unit = {
 //        findPerfectSquare();
 //        println(s"Factorial of 5 is : ${factorial(5, 1)}")  ;
+    getFibonacciNumber();
   }
 }
